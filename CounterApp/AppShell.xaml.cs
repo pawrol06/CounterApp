cb608a394fd8f9c08ce5b61e73f960dc4b756215
pawel <pawel@DESKTOP-460PL6C>
2024-12-04 21:20:32 +0100

    Dodaj pliki projektów.

---
+++ CounterApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CounterApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CounterApp/MainPage.xaml.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Maui.Controls;
+using System;
+
+namespace CounterApp;
+
+public partial class MainPage : ContentPage
+{
+    private int _counterIndex = 1; 
+
+    public MainPage()
+    {
+        InitializeComponent();
+        AddCounter($"Licznik {_counterIndex}"); 
+    }
+
+    
+    private void AddCounterClicked(object sender, EventArgs e)
+    {
+        _counterIndex++;
+        AddCounter($"Licznik {_counterIndex}");
+    }
+
+   
+    private void AddCounter(string counterName)
+    {
+        
+        var counterStack = new HorizontalStackLayout
+        {
+            Spacing = 10
+        };
+
+        
+        var nameLabel = new Label
+        {
+            Text = counterName,
+            VerticalOptions = LayoutOptions.Center
+        };
+
+       
+        var valueLabel = new Label
+        {
+            Text = "0",
+            VerticalOptions = LayoutOptions.Center,
+            HorizontalOptions = LayoutOptions.Center,
+            FontSize = 18,
+            WidthRequest = 50
+        };
+
+      
+        var decrementButton = new Button
+        {
+            Text = "-",
+            WidthRequest = 50
+        };
+        decrementButton.Clicked += (s, e) =>
+        {
+            int currentValue = int.Parse(valueLabel.Text);
+            valueLabel.Text = (currentValue - 1).ToString();
+        };
+
+        
+        var incrementButton = new Button
+        {
+            Text = "+",
+            WidthRequest = 50
+        };
+        incrementButton.Clicked += (s, e) =>
+        {
+            int currentValue = int.Parse(valueLabel.Text);
+            valueLabel.Text = (currentValue + 1).ToString();
+        };
+
+        
+        counterStack.Children.Add(nameLabel);
+        counterStack.Children.Add(decrementButton);
+        counterStack.Children.Add(valueLabel);
+        counterStack.Children.Add(incrementButton);
+
+        
+        CountersContainer.Children.Add(counterStack);
+    }
+}
